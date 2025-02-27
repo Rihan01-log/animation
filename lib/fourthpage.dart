b81c40191155bfb6c7647a91c420b16337920771
Rihan01-log <rihanrasheed201@gmail.com>
2025-02-27 13:30:57 +0530

    commit

--- lib/fourthpage.dart
+++ lib/fourthpage.dart
@@ -8,30 +8,36 @@ class Fourthpage extends StatefulWidget {
 }
 
 class _FourthpageState extends State<Fourthpage> {
-  double height=100;
- double width=100;
-  Color color=Colors.transparent;
+  double height = 100;
+  double width = 100;
+  Color color = Colors.transparent;
 
-  void changeSize(){
+  void changeSize() {
     setState(() {
-      height= height==100?100:200;
-      width =width==100?100:200;
-      color=color==Colors.transparent?Colors.red:Colors.blue;
+      height = height == 100 ? 200 : 100;
+      width = width == 100 ? 200 : 100;
+      color = color == Colors.transparent ? Colors.red : Colors.blue;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        AnimatedContainer(
-          height: height,
-          width: width,
-          color: color,
-          duration: Duration(seconds: 3)),
-          ElevatedButton(onPressed: () {
-            changeSize();
-          }, child: Text('Change'))
-      ],),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedContainer(
+              height: height,
+              width: width,
+              color: color,
+              duration: Duration(seconds: 3)),
+          ElevatedButton(
+              onPressed: () {
+                changeSize();
+              },
+              child: Text('Change'))
+        ],
+      ),
     );
   }
 }
